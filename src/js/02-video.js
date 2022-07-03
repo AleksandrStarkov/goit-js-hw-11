eaa5561b517922dfb174e3e2bf1a0aaa7ccc96e0
Aleksandr <102595282+AleksandrStarkov@users.noreply.github.com>
2022-07-03 21:56:37 +0300

    homwork8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const TIME_CURRENT_KEY = 'videoplayer-current-time';
+
+const onPlayTime = function (timeupdate) {
+  localStorage.setItem(TIME_CURRENT_KEY, JSON.stringify(timeupdate));
+  console.log(timeupdate);
+};
+player.on('timeupdate', throttle(onPlayTime, 1000));
+
+const timeStop = localStorage.getItem(TIME_CURRENT_KEY);
+// const pausedTime = JSON.parse(timeStop);
+if (timeStop) {
+  player.setCurrentTime(timeStop);
+}
+console.log(timeStop);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,32 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const textarea = document.querySelector('textarea');
+const LOCAL_TITLE_KEY = 'feedback-form-state';
+const input = document.querySelector('input');
+
+form.addEventListener('input', throttle(onInputText, 500));
+form.addEventListener('submit', onFormSubmit);
+
+const dataForm = {};
+
+function onInputText(event) {
+  dataForm[event.target.name] = event.target.value;
+  localStorage.setItem(LOCAL_TITLE_KEY, JSON.stringify(dataForm));
+}
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  event.currentTarget.reset();
+  localStorage.removeItem(LOCAL_TITLE_KEY);
+}
+
+function onFormData() {
+  const formData = JSON.parse(localStorage.getItem(LOCAL_TITLE_KEY));
+  if (formData) {
+    input.value = formData.email;
+    textarea.value = formData.message;
+    console.log(formData);
+  }
+}
+onFormData();
