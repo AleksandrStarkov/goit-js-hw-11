a07a74b3397d2aea663f8b85f8856f6ac931533c
Aleksandr <102595282+AleksandrStarkov@users.noreply.github.com>
2022-07-04 19:03:02 +0300

    fixed consol.log

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,22 +1,22 @@
 import throttle from 'lodash.throttle';
 
-const form = document.querySelector('.feedback-form');
-const textarea = document.querySelector('textarea');
+const form = document.querySelector('form');
+const textarea = document.querySelector('.feedback-form textarea');
 const LOCAL_TITLE_KEY = 'feedback-form-state';
 const input = document.querySelector('input');
 
 form.addEventListener('input', throttle(onInputText, 500));
 form.addEventListener('submit', onFormSubmit);
+
 onFormData();
-
-const dataForm = { email: `${input.value}`, message: `${textarea.value}` };
-
 function onInputText(event) {
+  const dataForm = { email: `${input.value}`, message: `${textarea.value}` };
   dataForm[event.target.name] = event.target.value;
   localStorage.setItem(LOCAL_TITLE_KEY, JSON.stringify(dataForm));
 }
 
 function onFormSubmit(event) {
+  console.log({ email: `${input.value}`, message: `${textarea.value}` });
   event.preventDefault();
   event.currentTarget.reset();
   localStorage.removeItem(LOCAL_TITLE_KEY);
