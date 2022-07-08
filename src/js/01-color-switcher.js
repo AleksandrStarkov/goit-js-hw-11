84ea8c523d4703fb5bf0a86d673d52b234ff5357
Aleksandr <102595282+AleksandrStarkov@users.noreply.github.com>
2022-07-08 16:43:15 +0300

    home work(9)

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+let timerId = null;
+
+btnStart.addEventListener('click', () => {
+  btnStart.setAttribute('disabled', 'disabled');
+  btnStop.removeAttribute('disabled', 'null');
+  timerId = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    btnStop.style.backgroundColor = getRandomHexColor();
+    btnStart.style.backgroundColor = getRandomHexColor();
+  }, 500);
+});
+btnStop.setAttribute('disabled', 'disabled');
+btnStop.addEventListener('click', () => {
+  btnStop.setAttribute('disabled', 'null');
+  clearInterval(timerId);
+  console.log(btnStop);
+  btnStart.removeAttribute('disabled', 'null');
+});
